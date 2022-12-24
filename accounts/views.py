3851275616a0678b6386a13dfe4ab787afe0a3dc
Ivan <jonnykold@mail.ru>
2022-12-24 22:01:46 +0100

    Add upload file view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views import View
+
+from accounts.forms import FileUploadForm
 
 
 class Account(View):
     template_name = 'accounts/account.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request=request, template_name=self.template_name)
+        file_upload_form: FileUploadForm = FileUploadForm()
+
+        return render(
+            request=request,
+            template_name=self.template_name,
+            context={
+                'file_upload_form': file_upload_form
+            }
+        )
+
+    def post(self, request, *args, **kwargs):
+        file_upload_form: FileUploadForm = FileUploadForm(
+            data=request.POST,
+            files=request.FILES,
+            request=request
+        )
+
+        if file_upload_form.is_valid():
+            file_upload_form.save()
+
+            return redirect(reverse('accounts:index'))
+
+        return render(
+            request=request,
+            template_name=self.template_name,
+            context={
+                'file_upload_form': file_upload_form
+            }
+        )
