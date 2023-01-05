a00aa3b5cd692e7f9501444676276937ec47a59d
Ivan <jonnykold@mail.ru>
2023-01-05 22:12:46 +0100

    Add template tag to get field verbose name

--- base/templatetags/extras.py
+++ base/templatetags/extras.py
@@ -1,4 +1,8 @@
+from typing import Type
+
 from django import template
+from django.core.exceptions import FieldDoesNotExist
+from django.db import models
 
 from core import settings
 
@@ -27,3 +31,21 @@ def get_setting(value: str):
         return settings.__getattribute__(setting_name)
     except AttributeError:
         raise RuntimeError('Setting %s is not found' % setting_name)
+
+
+@register.simple_tag
+def get_field_verbose_name(instance: Type[models.Model], field_name: str):
+    """Returns field verbose name.
+
+    Args:
+        instance (Type[models.Model]):
+        field_name
+
+    Returns:
+        str: Field verbose name.
+            Empty string if field not found.
+    """
+    try:
+        return instance._meta.get_field(field_name).verbose_name
+    except FieldDoesNotExist:
+        return ''
