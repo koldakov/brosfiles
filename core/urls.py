bc999e55ebacf3e427eddcba3720682f90755a4d
Ivan <jonnykold@mail.ru>
2023-02-05 22:52:32 +0100

    Remove unused imports

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView, TemplateView
+from django.views.generic import TemplateView
 
 from accounts.views import Account
 from api.views import Health
