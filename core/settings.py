9d6dbe8ddf742d77b72fe835c3c7774753c34cc6
Ivan <jonnykold@mail.ru>
2023-02-07 21:14:36 +0100

    Allow use different cloud storages

--- core/settings.py
+++ core/settings.py
@@ -186,10 +186,12 @@ elif AWS_STORAGE_BUCKET_NAME is not None:
     AWS_SECRET_ACCESS_KEY = ENV.get_value('AWS_SECRET_ACCESS_KEY')
     AWS_S3_REGION_NAME = ENV.get_value('AWS_S3_REGION_NAME')
 
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_S3_ADDRESSING_STYLE = 'virtual'
+
+    BF_CLOUD_STORAGE_SITE = ENV.get_value('BF_CLOUD_STORAGE_SITE', default=None)
+
+    if BF_CLOUD_STORAGE_SITE is not None:
+        AWS_S3_ENDPOINT_URL = 'https://s3.%s.%s' % (AWS_S3_REGION_NAME, BF_CLOUD_STORAGE_SITE)
 
     STATIC_URL = '/static/'
     STATIC_ROOT = str(BASE_DIR / 'static/')
