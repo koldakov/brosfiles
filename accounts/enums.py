41abeac3e14be17f4981a25933dd5f1043c22d8a
Ivan <jonnykold@mail.ru>
2023-02-08 19:50:12 +0100

    Remove unused objects

--- accounts/enums.py
+++ accounts/enums.py
@@ -8,22 +8,3 @@ class SignedURLMethod(Enum):
     PUT = 'PUT'
     GET = 'GET'
     RESUMABLE = 'RESUMABLE'
-
-
-class TransferType(Enum):
-    """Available transfer type"""
-    CHUNKED = 'CHUNKED'
-    SIGNED_URL = 'SIGNED_URL'
-    DEFAULT = 'DEFAULT'
-
-
-class UploadAction(Enum):
-    """Upload action"""
-    START = 'START'
-    FINISH = 'FINISH'
-
-
-class UploadStatus(Enum):
-    """Upload status"""
-    PENDING = 'PENDING'
-    DONE = 'DONE'
--- accounts/exceptions.py
+++ accounts/exceptions.py
@@ -1,10 +1,2 @@
 class UUID4HEXNotGenerated(Exception):
     pass
-
-
-class NotSupportedSignURLMethod(Exception):
-    pass
-
-
-class NotAllowed(Exception):
-    pass
