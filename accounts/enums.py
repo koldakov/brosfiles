774748c70863c0ff4ce3495d7a2cef1b0f60ed56
Ivan <jonnykold@mail.ru>
2023-03-05 23:11:34 +0100

    Add available file transfer types

--- accounts/enums.py
+++ accounts/enums.py
@@ -8,3 +8,10 @@ class SignedURLMethod(Enum):
     PUT = 'PUT'
     GET = 'GET'
     RESUMABLE = 'RESUMABLE'
+
+
+class TransferType(Enum):
+    """Available transfer type"""
+    CHUNKED = 'CHUNKED'
+    SIGNED_URL = 'SIGNED_URL'
+    DEFAULT = 'DEFAULT'
