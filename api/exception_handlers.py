6be3b13ce9ba4c4f2e76a60970fce393aa8dcf27
Ivan <jonnykold@mail.ru>
2023-04-12 00:13:08 +0200

    Fix exception handler

--- api/exception_handlers.py
+++ api/exception_handlers.py
@@ -5,7 +5,7 @@ from rest_framework.views import exception_handler
 
 def api_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    if response is not None:
+    if response is not None and 'detail' in response.data:
         response.data['message'] = response.data['detail']
         response.data['time'] = datetime.now()
         del response.data['detail']
