70467cfa71e856fbbb74db6ffe428a03cdae6693
Ivan <jonnykold@mail.ru>
2023-04-12 00:13:36 +0200

    Add time to all exceptions

--- api/exception_handlers.py
+++ api/exception_handlers.py
@@ -5,9 +5,9 @@ from rest_framework.views import exception_handler
 
 def api_exception_handler(exc, context):
     response = exception_handler(exc, context)
+    response.data['time'] = datetime.now()
     if response is not None and 'detail' in response.data:
         response.data['message'] = response.data['detail']
-        response.data['time'] = datetime.now()
         del response.data['detail']
 
     return response
