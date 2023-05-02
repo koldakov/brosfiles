fca844ef5af33d5c2789ba71a3a650fbb943856b
Ivan <jonnykold@mail.ru>
2023-05-02 21:22:54 +0200

    Add API exception

--- api/exceptions.py
+++ api/exceptions.py
@@ -25,3 +25,11 @@ class FeatureNotReady(BaseCustomException):
             detail = 'Not implemented'
         # Should be ``status.HTTP_501_NOT_IMPLEMENTED``, but webhook requires 200-299 response code.
         super().__init__(detail=detail, code=status.HTTP_200_OK)
+
+
+class UserAlreadyExists(BaseCustomException):
+    def __init__(self, detail=None):
+        if detail is None:
+            detail = 'User already exists'
+
+        super().__init__(detail=detail, code=status.HTTP_400_BAD_REQUEST)
