fb25960810d804f5355111fa6347d615a446b60c
Ivan <jonnykold@mail.ru>
2023-05-03 21:58:27 +0200

    Remove unused imports

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.models import update_last_login
 from django.db.utils import IntegrityError
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.settings import api_settings
 
 from accounts.models import User
 from api.exceptions import UserAlreadyExists
